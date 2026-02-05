78db3d1bb2e7a8638c6950d688a7a117853a2d1d
zuihaodi <4190008@qq.com>
2026-02-05 14:27:29 +0800

    m

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
-// astro.config.mjs
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  // 新增下面这段配置
+  i18n: {
+    defaultLocale: "zh", // 默认语言是中文
+    locales: ["zh", "en"], // 支持中文和英文
+    routing: {
+        prefixDefaultLocale: false // 中文版不强制加 /zh 前缀（直接访问域名就是中文）
+    }
+  }
 });
