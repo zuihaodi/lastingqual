11dcc66a92caa692f04fb28f413c054445143bed
zuihaodi <4190008@qq.com>
2026-04-20 00:36:13 +0800

    m

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,9 +8,10 @@ export const onRequest: MiddlewareHandler = async ({ url }, next) => {
   }
 
   const passthrough = new Response(res.body, res);
+  passthrough.headers.delete("Content-Security-Policy");
   passthrough.headers.set(
     "Content-Security-Policy",
-    "default-src 'self' 'unsafe-inline' 'unsafe-eval' https: data: blob:;"
+    "default-src 'self' https: data: blob:; script-src 'self' 'unsafe-inline' 'unsafe-eval' https:; connect-src 'self' https:; img-src 'self' https: data: blob:; style-src 'self' 'unsafe-inline' https:; font-src 'self' https: data:; frame-ancestors 'self'; base-uri 'self';"
   );
   passthrough.headers.set("X-Frame-Options", "SAMEORIGIN");
   return passthrough;
