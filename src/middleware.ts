d56bc52c837a03bcac7d83043ca27523e87a0dcb
zuihaodi <4190008@qq.com>
2026-04-20 12:57:31 +0800

    m

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,3 @@
 import type { MiddlewareHandler } from "astro";
 
-export const onRequest: MiddlewareHandler = async ({ url }, next) => {
-  const res = await next();
-
-  if (!url.pathname.startsWith("/keystatic") && !url.pathname.startsWith("/api/keystatic")) {
-    return res;
-  }
-
-  res.headers.delete("Content-Security-Policy");
-  res.headers.set(
-    "Content-Security-Policy",
-    "default-src 'self' https: data: blob:; script-src 'self' 'unsafe-inline' 'unsafe-eval' https:; connect-src 'self' https:; img-src 'self' https: data: blob:; style-src 'self' 'unsafe-inline' https:; font-src 'self' https: data:; frame-ancestors 'self'; base-uri 'self';"
-  );
-  res.headers.set("X-Frame-Options", "SAMEORIGIN");
-  return res;
-};
+export const onRequest: MiddlewareHandler = async (_ctx, next) => next();
