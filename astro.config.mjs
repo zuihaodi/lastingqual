bf06b7ef8e04570c77dd2f51eb4829c16952a5fa
zuihaodi <4190008@qq.com>
2026-04-20 22:53:12 +0800

    m

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
-import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
 
 export default defineConfig({
   output: "server",
   adapter: cloudflare(),
-  integrations: [tailwind(), react()],
+  integrations: [],
   vite: {
     cacheDir: ".vite-local",
   },
